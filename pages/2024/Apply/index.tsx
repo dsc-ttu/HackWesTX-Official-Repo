a5137b082506df7811b9186eadf1f18ee6dac16c
aavasare <aniketavasare2004@gmail.com>
2024-08-12 12:52:24 -0500

    errors fixed

--- pages/2024/Apply/index.tsx
+++ pages/2024/Apply/index.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const RegistrationPage = () => {
     return (
-        <Grid container overflow="clip">
+        <Grid container>
             <div className="flex flex-col justify-center w-full h-full gap-y-5">
                 <div className="my-4 flex flex-col justify-center text-center font-light text-4xl w-3/4 gap-6 mx-auto">
                     <div>
@@ -21,7 +21,7 @@ const RegistrationPage = () => {
                     <iframe
                         id="ts-iframe"
                         src="https://www.townscript.com/v2/widget/hackwestx-v-2024-113224/booking"
-                        frameBorder="0"
+                        
                         height="600"
                         width="80%"
                         title="Townscript Booking"
@@ -32,12 +32,12 @@ const RegistrationPage = () => {
                 <div className="flex flex-col mx-auto w-4/5 my-8">
                     <div className="text-center font-light text-3xl w-3/4 gap-6 mx-auto my-8">
                         For any additional questions,{" "}
-                        <a
+                        <Link
                             className="text-yellow-700 m-0 p-0"
                             href="mailto:contact@hackwestx.tech"
                         >
                             contact us.
-                        </a>
+                        </Link>
                     </div>
                 </div>
             </div>
